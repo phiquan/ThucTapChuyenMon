2b34dfa144b8afa0a1d3b36ec62b627c4eb0edd6
phiquan <80434542+phiquan@users.noreply.github.com>
2021-05-28 12:49:44 +0700

    createThreeLayer

---
+++ project/DAO/DataProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance 
+        {
+            get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
+            private set { DataProvider.instance = value; }
+        }
+
+       
+
+        private string connStr = "Data Source=DESKTOP-Q4GRQFB\\SQLEXPRESS;Initial Catalog=TTCM;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            SqlConnection connection = new SqlConnection(connStr);
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable data = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(data);
+
+            connection.Close();
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
---
+++ project/DAO/LoginDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.DAO
+{
+    public class LoginDAO
+    {
+        private static LoginDAO instance;
+
+        public static LoginDAO Instance
+        {
+            get { if (instance == null) instance = new LoginDAO(); return instance; }
+            private set { instance = value; }
+        }
+
+        private LoginDAO() { }
+
+        public bool Login(string userName, string passWord)
+        {
+            string query = "SELECT * FROM Staff WHERE Email='" + userName + "' AND Pass='" + passWord + "'";
+            DataTable result = DataProvider.Instance.ExecuteQuery(query);
+            return result.Rows.Count > 0;
+        }
+    }
+}
